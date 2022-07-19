346e487d9f98963439a5394f4f6ab50fc4327af9
Jessica Greene <jessica.greene89@gmail.com>
2022-07-19 14:44:33 -0400

    Moved controller to controllers package

--- Week3/DaikichiPathVariables/daikichipathvariables/src/main/java/com/jessicagreene/daikichipathvariables/controllers/DaikichiController.java
+++ Week3/DaikichiPathVariables/daikichipathvariables/src/main/java/com/jessicagreene/daikichipathvariables/controllers/DaikichiController.java
@@ -1,4 +1,4 @@
-package com.jessicagreene.daikichipathvariables;
+package com.jessicagreene.daikichipathvariables.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
