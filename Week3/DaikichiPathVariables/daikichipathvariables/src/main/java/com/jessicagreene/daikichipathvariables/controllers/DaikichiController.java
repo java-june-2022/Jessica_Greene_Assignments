5a0104d9cfb7727a38cf48efe2db9acf354dde6a
Jessica Greene <jessica.greene89@gmail.com>
2022-07-21 14:51:13 -0400

    added Daikichi Path Variables

--- Week3/DaikichiPathVariables/daikichipathvariables/src/main/java/com/jessicagreene/daikichipathvariables/controllers/DaikichiController.java
+++ Week3/DaikichiPathVariables/daikichipathvariables/src/main/java/com/jessicagreene/daikichipathvariables/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.jessicagreene.daikichipathvariables.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,19 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return String.format("Congratulations! You will soon travel to %s!", city);
+	}
+	
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") int num) {
+		if (num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 	}
 }
